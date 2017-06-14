63e360ec0f61324eb21ae0bb9d9e64582810eb77
July Manova <jmanova83@gmail.com>
2017-06-14 22:34:22 +0300

    80/100 bombs

--- BombNumbers/Program.cs
+++ BombNumbers/Program.cs
@@ -10,34 +10,30 @@ namespace BombNumbers
 	{
 		static void Main(string[] args)
 		{
-			List<int> nums = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
-			int[] bomb = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
-			int bombNum = bomb[0];
-			int power = bomb[1];
+			List<long> nums = Console.ReadLine().Split(' ').Select(long.Parse).ToList();
+			long[] bomb = Console.ReadLine().Split(' ').Select(long.Parse).ToArray();
+			long bombNum = bomb[0];
+			int power = (int)bomb[1];
 
 			for (int i = 0; i < nums.Count; i++)
 			{
 				if (nums[i] == bombNum)
 				{
-					if (i + power < nums.Count - 1 && i - power >= 0)
+					for (int j = 0; j < power * 2 + 1; j++)
 					{
-						for (int j = 0; j < power * 2 + 1; j++)
+						if (i - power + j >= 0)
 						{
-							nums.Remove(nums[i - power]);
-						}
-					}
-					else if (i + power >= nums.Count - 1)
-					{
-						for (int j = 0; j < nums.Count; j++)
-						{
-							nums.Remove(nums[i - power]);
-						}
-						nums.Remove(nums[nums.Count - 1]);
-					} else if (i - power < 0)
-					{
-						for (int k = 0; k < power + 1; k++)
-						{
-							nums.Remove(nums[0]);
+							if (i - power < nums.Count)
+							{
+								if (i - power >= 0)
+								{
+									nums.Remove(nums[i - power]);
+								}
+								else if (nums.Count > 0)
+								{
+									nums.Remove(nums[0]);
+								}
+							}
 						}
 					}
 				}
