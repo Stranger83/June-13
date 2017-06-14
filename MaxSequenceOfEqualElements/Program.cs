7fe8956dc100a05acd41970cd1cc6432291d68f8
Stranger <jmanova83@gmail.com>
2017-06-14 13:56:33 +0300

    First exercise done

--- MaxSequenceOfEqualElements/Program.cs
+++ MaxSequenceOfEqualElements/Program.cs
@@ -10,6 +10,33 @@ namespace MaxSequenceOfEqualElements
 	{
 		static void Main(string[] args)
 		{
+			List<int> nums = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+			int bestLength = 1;
+			int bestPos = 0;
+			int currentBestLenght = 1;
+			for (int pos = 0; pos < nums.Count; pos++)
+			{
+				if (pos < nums.Count - 1 && nums[pos] == nums[pos + 1])
+				{
+					currentBestLenght++;
+				}
+				else
+				{
+					currentBestLenght = 1;
+				}
+
+				if (currentBestLenght > bestLength)
+				{
+					bestLength = currentBestLenght;
+					bestPos = pos;
+				}
+
+			}
+			for (int i = 0; i < bestLength; i++)
+			{
+				Console.Write($"{nums[bestPos]} ");
+			}
+			Console.WriteLine();
 		}
 	}
 }
