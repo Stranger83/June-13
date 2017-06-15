99b37d711b21930efa187b7ad67f82011f449306
Stranger <jmanova83@gmail.com>
2017-06-15 22:40:47 +0300

    all done

--- ArrayManipulator/Program.cs
+++ ArrayManipulator/Program.cs
@@ -11,37 +11,26 @@ namespace ArrayManipulator
 		static void Main(string[] args)
 		{
 			List<int> nums = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
-					
+			string input = Console.ReadLine();		
 
-			while (true)
+			while (input != "print")
 			{
-				List<string> input = Console.ReadLine().Split(' ').ToList();
-				string command = input[0];
-				
-				if (command == "print")
-				{
-					break;
-				}
-
-				List<int> parameters = new List<int>();
-				for (int i = 1; i < input.Count; i++)
-				{
-					parameters.Add(int.Parse(input[i]));
-				}
+				var parameters = input.Split();
+				var command = parameters[0];
 
 				switch (command)
 				{
 					case "add":
-						nums.Insert(parameters[0], parameters[1]);
+						nums.Insert(int.Parse(parameters[1]), int.Parse(parameters[2]));
 						break;
 					case "addMany":
 						AddMany(nums, parameters);
 						break;
 					case "contains":
-						Console.WriteLine(nums.IndexOf(parameters[0]));
+						Console.WriteLine(nums.IndexOf(int.Parse(parameters[1])));
 						break;
 					case "remove":
-						nums.RemoveAt(parameters[0]);
+						nums.RemoveAt(int.Parse(parameters[1]));
 						break;
 					case "shift":
 						ShiftLeft(nums, parameters);
@@ -50,7 +39,7 @@ namespace ArrayManipulator
 						nums = SumPairs(nums);
 						break;
 				}
-				
+				input = Console.ReadLine();
 			}
 			Console.WriteLine($"[{string.Join(", ", nums)}]");
 		}
@@ -60,36 +49,36 @@ namespace ArrayManipulator
 			List<int> summedPairs = new List<int>();
 			for (int i = 0; i < nums.Count; i += 2)
 			{
-				if (i == nums.Count - 1)
+				int currentElement = nums[i];
+				int nextElement = 0;
+				if (i < nums.Count - 1)
 				{
-					summedPairs.Add(nums[i]);
+					nextElement = nums[i + 1];
 				}
-				else
-				{
-					summedPairs.Add(nums[i] + nums[i + 1]);
-				}
+				summedPairs.Add(currentElement + nextElement);
 			}
 			nums = summedPairs;
 			return nums;
 		}
 
-		private static void ShiftLeft(List<int> nums, List<int> parameters)
+		private static void ShiftLeft(List<int> nums, string[] parameters)
 		{
-			for (int i = 0; i < (parameters[0] % nums.Count); i++)
+			int count = int.Parse(parameters[1]) % nums.Count;
+			for (int i = 0; i < count; i++)
 			{
 				nums.Add(nums[0]);
 				nums.RemoveAt(0);
 			}
 		}
 
-		private static void AddMany(List<int> nums, List<int> parameters)
+		private static void AddMany(List<int> nums, string[] parameters)
 		{
 			List<int> numsToAdd = new List<int>();
-			for (int i = 1; i < parameters.Count; i++)
+			for (int i = 2; i < parameters.Length; i++)
 			{
-				numsToAdd.Add(parameters[i]);
+				numsToAdd.Add(int.Parse(parameters[i]));
 			}
-			nums.InsertRange(parameters[0], numsToAdd);
+			nums.InsertRange(int.Parse(parameters[1]), numsToAdd);
 		}
 	}
 }
